6a82230fabeabdb71d24552f49f6b22d6ca6baf1
GvN1989 <geesje.noordenburg@gmail.com>
2025-01-11 15:37:54 +0100

    first commit

---
+++ src/main/java/nl/novi/bloomtrail/BloomTrailApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.bloomtrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomTrailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomTrailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/bloomtrail/BloomTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.bloomtrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomTrailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
