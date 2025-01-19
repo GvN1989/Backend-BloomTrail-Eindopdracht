eb3b1122084a85ac031a3cb430cb710861366e01
GvN1989 <geesje.noordenburg@gmail.com>
2025-01-19 21:41:54 +0100

    also added the test files to java test

---
+++ src/test/java/nl/novi/bloomtrail/dtos/SessionInputDtoTest.java
@@ -0,0 +1,7 @@
+package nl.novi.bloomtrail.dtos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionInputDtoTest {
+
+}
---
+++ src/test/java/nl/novi/bloomtrail/mappers/AssignmentMapperTest.java
@@ -0,0 +1,7 @@
+package nl.novi.bloomtrail.mappers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssignmentMapperTest {
+
+}
