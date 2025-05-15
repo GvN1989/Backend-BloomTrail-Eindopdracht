0a493117deb5e94918e0a8ea2af64702f57bc8bb
GvN1989 <geesje.noordenburg@gmail.com>
2025-05-15 12:59:06 +0200

    deleted some unused import statements

--- src/test/java/nl/novi/bloomtrail/utils/SecurityTestHelper.java
+++ src/test/java/nl/novi/bloomtrail/utils/SecurityTestHelper.java
@@ -1,12 +1,8 @@
 package nl.novi.bloomtrail.utils;
 
-import nl.novi.bloomtrail.models.User;
-import nl.novi.bloomtrail.models.Authority;
-import org.mockito.Mockito;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.List;
